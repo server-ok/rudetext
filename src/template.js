80cbf3f63a8a094110cfed47054d74bf0cb176d8
serverok <68389045+server-ok@users.noreply.github.com>
2024-08-04 19:42:45 +0300

    Update template.js

--- src/template.js
+++ src/template.js
@@ -3,7 +3,7 @@
 export default function template(configgers) {
 
     var element = `
-        <svg width="${configgers.font_size*configgers.text.length}" height="${configgers.font_size}" viewBox="0 0 500 200" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+        <svg width="${configgers.font_size*configgers.text.length/2}" height="${configgers.font_size}" viewBox="0 0 500 200" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
         <style>
 
             @font-face {
