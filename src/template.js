8e4031963aedd5edd7e85e19c638a2c2a13ab40a
serverok <68389045+server-ok@users.noreply.github.com>
2024-08-04 18:41:44 +0300

    the

--- src/template.js
+++ src/template.js
@@ -2,17 +2,22 @@
 
 export default function template(configgers) {
     var element = `
-        <svg width="495" height="195" viewBox="0 0 495 195" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+        <svg width="500" height="200" viewBox="0 0 500 200" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
         <style>
 
+            @font-face {
+                font-family: punky;
+                src: url("fonts/Punky Wunky.ttf");
+            }
+
             .THEtext {
-                font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+                font-family: '${configgers.font}', 'Segoe UI', 'Tahoma', 'Geneva', 'Verdana', 'sans-serif';
                 fill: #${configgers.text_color};
                 font-size: 14px;
             }
 
         </style>
-    <text class="THEtext">#${configgers.text}</text>
+    <text class="THEtext" x="250" y="100">${configgers.text}</text>
     </svg>
     `
 
