62bc71f01cc3ef2d7bd57698427c5c3b117677ac
serverok <68389045+server-ok@users.noreply.github.com>
2024-08-04 19:41:17 +0300

    Update template.js

--- src/template.js
+++ src/template.js
@@ -1,14 +1,9 @@
 
 
 export default function template(configgers) {
-    var parser = new DOMParser();
-
-    var text_element = `<text class="THEtext" x="50%" y="50%" dominant-baseline="middle" text-anchor="middle">${configgers.text}</text>`
-    var doc = parser.parseFromString(stringContainingXMLSource, "image/svg+xml");
-    doc.documentElement.getComputedTextLength();
 
     var element = `
-        <svg width="500" height="200" viewBox="0 0 500 200" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+        <svg width="${configgers.font_size*configgers.text.length}" height="${configgers.font_size}" viewBox="0 0 500 200" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
         <style>
 
             @font-face {
@@ -65,7 +60,7 @@ export default function template(configgers) {
 
 
         </style>
-    <text class="THEtext" x="50%" y="50%" dominant-baseline="middle" text-anchor="middle">${doc.documentElement.getComputedTextLength()}</text>
+    <text class="THEtext" x="50%" y="50%" dominant-baseline="middle" text-anchor="middle">${configgers.text}</text>
     </svg>
     `
 
