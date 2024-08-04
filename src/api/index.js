b4c77631fbc15fcee26a2c4c6a8bfcf9f0ed4831
serverok <68389045+server-ok@users.noreply.github.com>
2024-08-04 18:50:27 +0300

    animation and

--- src/api/index.js
+++ src/api/index.js
@@ -7,7 +7,10 @@ export default async function rudeText(req, res) {
     let configgers = {
         "text": req.query.text || "Default text. You better set it to something lol.",
         "text_color": req.query.text_color || "FFFFFF",
-        "animation": req.query.anim_name || "fall"
+        "animation": req.query.anim_name || "fall",
+        "delay": req.query.delay || 0,
+        "font": req.query.font || "Segoe UI",
+        "font_size": req.query.fontsize || 16
     }
 
     let anims = ["fall"]
--- src/template.js
+++ src/template.js
@@ -13,8 +13,49 @@ export default function template(configgers) {
             .THEtext {
                 font-family: '${configgers.font}', 'Segoe UI', 'Tahoma', 'Geneva', 'Verdana', 'sans-serif';
                 fill: #${configgers.text_color};
-                font-size: 14px;
+                font-size: ${configgers.font_size}px;
+                animation-name: ${configger.animation};
+                animation-duration: 0.5s;
+                animation-timing-function: ease-in;
+                animation-fill-mode: forwards;
+                animation-delay: ${configger.delay};
             }
+
+            @keyframes fall {
+                0% {
+                    color: transparent;
+                    transform: scale(40, 40);
+                }
+                50% {
+                    color: #${configgers.text_color};
+                    transform: scale(1, 1);
+                }
+                55% {
+                    color: #${configgers.text_color};
+                    transform: translate(10px, 10px);
+                }
+                60% {
+                    color: red;
+                    transform: translate(-10px, 5px);
+                }
+                70% {
+                    color: red;
+                    transform: translate(5px, 10px);
+                }
+                80% {
+                    color: #${configgers.text_color};
+                    transform: translate(-5px, 2px);
+                }
+                90% {
+                    color: red;
+                    transform: translate(3px, 3px);
+                }
+                100% {
+                    color: #${configgers.text_color};
+                    transform: translate(2px, 2px);
+                }
+            }
+
 
         </style>
     <text class="THEtext" x="250" y="100">${configgers.text}</text>
