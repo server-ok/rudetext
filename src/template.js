b7797889a51146bd1f79a0c119042e82b6da28e2
serverok <68389045+server-ok@users.noreply.github.com>
2024-08-06 15:42:45 +0300

    Update template.js

--- src/template.js
+++ src/template.js
@@ -1,79 +1,79 @@
 import { punky } from "./fonts/base64.js"
 
-const animations = {
-    "fall":
-        `@keyframes fall {
-                0% {
-                    fill: transparent;
-                    transform: scale(20, 20);
-                }
-                50% {
-                    fill: #${configgers.text_color};
-                    transform: scale(1, 1);
-                }
-                55% {
-                    fill: #${configgers.text_color};
-                    transform: translate(10px, 10px);
-                }
-                60% {
-                    fill: red;
-                    transform: translate(-10px, 5px);
-                }
-                70% {
-                    fill: red;
-                    transform: translate(5px, 10px);
-                }
-                80% {
-                    fill: #${configgers.text_color};
-                    transform: translate(-5px, 2px);
-                }
-                90% {
-                    fill: red;
-                    transform: translate(3px, 3px);
-                }
-                100% {
-                    fill: #${configgers.text_color};
-                    transform: translate(2px, 2px);
-                }
-            }`,
-    "rainbow":
-        `@keyframes rainbow {
-                0% {
-                    fill: red;
-                }
-
-                14% {
-                    fill: orange;
-                }
-
-                28% {
-
-                    fill: yellow;
-                }
-
-                42% {
-                    fill: green;
-                }
-
-                56% {
-                    fill: cyan;
-                }
-
-                70% {
-                    fill: blue;
-                }
-
-                84% {
-                    fill: purple;
-                }
-
-                100% {
-                    fill: red;
-                }
-            }`
-}
 
 export default function template(configgers) {
+    const animations = {
+        "fall":
+            `@keyframes fall {
+                    0% {
+                        fill: transparent;
+                        transform: scale(20, 20);
+                    }
+                    50% {
+                        fill: #${configgers.text_color};
+                        transform: scale(1, 1);
+                    }
+                    55% {
+                        fill: #${configgers.text_color};
+                        transform: translate(10px, 10px);
+                    }
+                    60% {
+                        fill: red;
+                        transform: translate(-10px, 5px);
+                    }
+                    70% {
+                        fill: red;
+                        transform: translate(5px, 10px);
+                    }
+                    80% {
+                        fill: #${configgers.text_color};
+                        transform: translate(-5px, 2px);
+                    }
+                    90% {
+                        fill: red;
+                        transform: translate(3px, 3px);
+                    }
+                    100% {
+                        fill: #${configgers.text_color};
+                        transform: translate(2px, 2px);
+                    }
+                }`,
+        "rainbow":
+            `@keyframes rainbow {
+                    0% {
+                        fill: red;
+                    }
+
+                    14% {
+                        fill: orange;
+                    }
+
+                    28% {
+
+                        fill: yellow;
+                    }
+
+                    42% {
+                        fill: green;
+                    }
+
+                    56% {
+                        fill: cyan;
+                    }
+
+                    70% {
+                        fill: blue;
+                    }
+
+                    84% {
+                        fill: purple;
+                    }
+
+                    100% {
+                        fill: red;
+                    }
+                }`
+    }
 
     var element = `
         <svg width="${configgers.width}" height="${configgers.height}" viewBox="0 0 ${configgers.width} ${configgers.height}" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
