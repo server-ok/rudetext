4de3b37f947f6453d1bc2064e9b5e842ee5ed340
serverok <68389045+server-ok@users.noreply.github.com>
2024-08-07 00:30:28 +0300

    let to var (var went to bar)

--- src/template.js
+++ src/template.js
@@ -77,7 +77,7 @@ template = function(configgers) {
     }
 
     if (configgers.animation == "wave") {
-        let sinMatrix = Array.from(Array(5), () => new Array(configgers.text.length));
+        var sinMatrix = Array.from(Array(5), () => new Array(configgers.text.length));
         for (let frame=0;frame<5;frame++) {
             for (let c=0;c<configgers.text.length;c++){
                 sinMatrix[frame][c] = Math.round(Math.sin(c+1)*8-Math.sin(c)*8);
